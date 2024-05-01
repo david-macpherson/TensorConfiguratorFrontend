c1d002438f144b840be743f7d8db6fea3b6fbf3a
David MacPherson <david@tensorworks.com.au>
2024-05-01 11:52:54 +1000

    Removed building constants

--- CarConfigurator/src/const.ts
+++
@@ -1,8 +0,0 @@
-// For SPS version 0.10.2 and below the websocket address required to connect had a suffix of /ws added to the end of the path
-// eg ws://<hostname>/<application name>/ws 
-// For SPS versions above 0.10.2 the websocket address was changed to connect to /signalling and then the pathname was added
-// eg ws://<hostname>/signalling/<application name>
-// Set this to true if building for use on SPS versions v0.10.2 or below
-export var SPS_LEGACY_SIGNALLING_PATH_WS: boolean = false;
-
-
